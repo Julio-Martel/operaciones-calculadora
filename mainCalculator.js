110270d7e8580b596db28ad1481148dde715ffd9
Diocleniax789 <julioramdom123@gmail.com>
2025-04-24 22:35:36 -0300

    se ha agregado un comentario

---
+++ mainCalculator.js
@@ -0,0 +1,11 @@
+// MODULOS 
+
+const suma = require('./operaciones/suma.js');
+const resta = require('./operaciones/resta.js');
+const producto = require('./operaciones/producto.js');
+const division = require('./operaciones/division.js');
+
+console.log(suma.suma(5,7));
+console.log(resta.resta(6,2));
+console.log(producto.producto(2,5));
+console.log(division.division(8,6));
