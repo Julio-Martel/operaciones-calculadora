d5f5d0af9aba37bf90842b965523206b037a1162
Diocleniax789 <julioramdom123@gmail.com>
2025-04-27 21:31:14 -0300

    se han agregado nuevas variables

--- mainCalculator.js
+++ mainCalculator.js
@@ -1,11 +1,12 @@
 // MODULOS 
-
 const suma = require('./operaciones/suma.js');
 const resta = require('./operaciones/resta.js');
 const producto = require('./operaciones/producto.js');
 const division = require('./operaciones/division.js');
+const operacionMultiple = require('./operaciones/operacionMultiple.js');
 
 console.log(suma.suma(5,7));
 console.log(resta.resta(6,2));
 console.log(producto.producto(2,5));
 console.log(division.division(8,6));
+console.log(operacionMultiple.operacionMultiple(8,6,81));
